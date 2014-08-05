02016062f175cead788d996cb055853bce52663b
Eric Schles <ericschles@gmail.com>
2014-08-04 21:14:32 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMembers
+{
+    public static class Rectangle
+    {
+        public static int CalculateArea(int width, int height)
+        {
+            return width * height;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The area is: " + Rectangle.CalculateArea(5, 4));
+            Console.ReadKey();
+
+        }
+    }
+}
